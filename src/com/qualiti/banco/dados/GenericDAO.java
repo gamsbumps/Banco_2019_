acdf4ec0224967b7e2d3e9d940fbefd24093a5e5
Guilherme <guilhermecc2@hotmail.com>
2019-02-15 22:01:56 -0300

    Banco de dados cliente

--- src/com/qualiti/banco/dados/GenericDAO.java
+++ src/com/qualiti/banco/dados/GenericDAO.java
@@ -1,9 +1,11 @@
 package com.qualiti.banco.dados;
 
+import com.qualiti.banco.excecoes.BancoException;
+
 public interface GenericDAO<G, CHAVE> { //G é o cliente ou conta.CHAVE é a chave primaria
-	void inserir(G entity); 
+	void inserir(G entity) throws BancoException; 
 	void atualizar(G entity);
-	void remover(CHAVE chave);
+	void remover(CHAVE chave) throws BancoException;
 	G procurar(CHAVE chave);
 
 }
